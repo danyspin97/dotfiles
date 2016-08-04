339ae7c9143bff719f8a91bf8b6cf031f54bb3d7
Dany Spin <danyspin97@danyspin-pc.homenet.telecomitalia.it>
2016-08-04 18:52:21 +0200

    Added Homemaker support and varius changes

--- install_arch.sh
+++ install_arch.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/bash
-cd
+sudo pacman -Syu
+sudo pacman -S yaourt git --noconfirm
 wget https://foosoft.net/projects/homemaker/dl/homemaker_linux_amd64.tar.gz
 tar -zxf homemaker_linux_amd64.tar.gz
 cd homemaker_linux_amd64
 sudo mv homemaker /usr/bin/
 cd
-git clone https://gitlab.com/danyspin97/dotfiles
 cd dotfiles
 homemaker -clobber --variant=desktop config.toml .

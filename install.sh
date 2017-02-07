0793240a038a91061e9f1a4e6e72d526535e96b6
Danilo Spinella <danyspin.ds@gmail.com>
2017-02-07 16:33:12 +0100

    Install yaourt in bash, change config.toml

--- install.sh
+++ install.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/bash
-wget https://foosoft.net/projects/homemaker/dl/homemaker_linux_amd64.tar.gz
-tar -zxf homemaker_linux_amd64.tar.gz
-cd homemaker_linux_amd64
-sudo mv homemaker /usr/bin/
-cd
-cd dotfiles
+which homemaker
+if [ $? -eq 0  ]
+then
+	echo "Homemaker is already installed"
+else
+	wget https://foosoft.net/projects/homemaker/dl/homemaker_linux_amd64.tar.gz
+	tar -zxf homemaker_linux_amd64.tar.gz
+	cd homemaker_linux_amd64
+	sudo mv homemaker /usr/bin/
+	cd
+	cd dotfiles
+fi
--- install_arch.sh
+++ install_arch.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/bash
 sudo pacman -Syu
+git clone https://aur.archlinux.org/package-query.git
+cd package-query
+makepkg -si
+cd ..
+git clone https://aur.archlinux.org/yaourt.git
+cd yaourt
+makepkg -si
+curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
+     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 ./install.sh
-homemaker -clobber --variant=desktop config.toml .
+homemaker -clobber --variant=arch config.toml .

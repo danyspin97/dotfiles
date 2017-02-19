104519040576f7c7d08bc330557268242a5f4344
Danilo Spinella <danyspin.ds@gmail.com>
2017-02-19 18:04:28 +0100

    Update keepass_sync to work from any directory

--- .scripts/volume.fish
+++
@@ -1,3 +0,0 @@
-amixer sget Master | gawk '/%/ {gsub(/[\[\]%]/, "", $5); volume = $5} END { if ($6 == "[off]") print "  Muted"
-									   else if ($6 == "[on]") if (volume > 35) print " " volume "%"
-												  else print "  " volume "%"}'
